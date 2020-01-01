ebfee6e0038fe1dd5e2b87f81a61b9722f499b07
Kai Zhang <kai.zhangxa@gmail.com>
2020-01-01 19:58:00 +0100

    Put qr code on a card

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       <div class="w-full" v-show="showForm">
         <WifiQRCodeForm></WifiQRCodeForm>
       </div>
-      <div class="mx-auto mb-4">
+      <div class="mx-auto mb-4 shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <QrcodeVue v-if="passString" :value="passString" :size="300"></QrcodeVue>
       </div>
       <p class="text-center text-sm text-gray-700 w-screen">built by <a href="https://twitter.com/bearzk">@bearzk</a></p>
